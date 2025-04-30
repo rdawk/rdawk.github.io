c33bd40e60912cbd1c9f33b0d315d12c8eda28e2
Ryan Dawkin <38119762+rdawk@users.noreply.github.com>
2025-04-30 14:44:07 -0400

    Add files via upload

---
+++ projects/IA4/script.js
@@ -0,0 +1,27 @@
+function steelerAbout(){
+    var st = document.getElementById("steelerabout");
+    if (st.style.display === "none"){
+        st.style.display = "block";
+    }
+        else{
+            st.style.display = "none";
+        }
+    }
+function pirateAbout(){
+        var pi = document.getElementById("pirateabout");
+        if (pi.style.display === "none"){
+            pi.style.display = "block";
+        }
+            else{
+                pi.style.display = "none";
+            }
+    }
+function pensAbout(){
+        var pen = document.getElementById("pensabout");
+        if (pen.style.display === "none"){
+            pen.style.display = "block";
+        }
+            else{
+                pen.style.display = "none";
+            }
+    }
